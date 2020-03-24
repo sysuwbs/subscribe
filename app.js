a7bd1c86db8b2b7c24f14d426f82e2adcd1142af
sysuwbs <sysuwbs@gmail.com>
2020-03-24 22:01:46 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * 入口文件
+ */
+
+/**
+ * 开发依赖
+ */
+let path = require('path')
+let util = require('./util')
+let base64 = util.base64
+let readSync = util.readSync
+let writeSync = util.writeSync
+
+let BUILD_DIR = 'netlify' // 构建目录
+let ENTRY_FILE = './source.txt' //  nodes
+let str = readSync(ENTRY_FILE)
+
+let checker = item => {
+    return item => item.includes('ssr://') || item.includes('ss"//')|| item.includes('vmess"//')
+}
+
+// Map all the item include `ssr://` and serialize those items
+let result = str.split('\n\n')
+                .filter(item => checker(item))
+                .join('\r\n')
+
+writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
